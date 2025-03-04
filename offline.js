9cd63d827889cb1073cef0ac2221c6cf0a83c4f3
meditationSounds <youcataclysm@gmail.com>
2025-03-04 16:16:47 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1741094165,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/particles.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite2-sheet0.png",
+		"images/13e2d9cd0e274ac4a34e04a9a-sheet0.png",
+		"images/particles2.png",
+		"media/bowl2.ogg",
+		"media/rain.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
